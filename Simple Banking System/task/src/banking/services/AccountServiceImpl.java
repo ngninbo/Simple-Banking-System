e7da9d1ed3f41dba7f4094d8c95ae7a4d19aa8a8
Beauclair Ngnintedem <beauclair.dongmo.ngnintedem@cofinpro.de>
2022-01-09 13:17:40 +0100

    CHG: Implement and use functional interface Predicate

--- Simple Banking System/task/src/banking/services/AccountServiceImpl.java
+++ Simple Banking System/task/src/banking/services/AccountServiceImpl.java
@@ -2,14 +2,24 @@ package banking.services;
 
 import banking.util.DisplayMessage;
 
+import java.util.function.Predicate;
+
 public class AccountServiceImpl implements AccountService {
 
     private final String cardNumber;
     private final CardService cardService;
 
+    private final Predicate<String> isNotPresent;
+    private final Predicate<String> isValid = CardService::isValid;
+
     public AccountServiceImpl(String cardNumber, CardService cardService) {
         this.cardNumber = cardNumber;
         this.cardService = cardService;
+        isNotPresent = cardNumberPresentChecker().negate();
+    }
+
+    private Predicate<String> cardNumberPresentChecker() {
+        return cardService::isCardByNumberPresent;
     }
 
     @Override
@@ -25,13 +35,13 @@ public class AccountServiceImpl implements AccountService {
         System.out.println(DisplayMessage.TRANSFER_TEXT);
         targetCardNumber = RequestUserTo.inputTargetCardNumber();
 
-        if (!CardService.isValid(targetCardNumber)) {
+        if (isValid.negate().test(targetCardNumber)) {
             System.out.println(DisplayMessage.CARD_NUMBER_ERROR_MSG);
         } else {
             if (targetCardNumber.equals(cardNumber)) {
                 System.out.println(DisplayMessage.SAME_ACCOUNT_ERROR_MSG);
             } else {
-                if (!isCardByNumberPresent(targetCardNumber)) {
+                if (isNotPresent.test(targetCardNumber)) {
                     System.out.println(DisplayMessage.CARD_NOT_EXISTS_ERROR_MSG);
                 } else {
                     long amount = RequestUserTo.inputAmount(DisplayMessage.AMOUNT_TO_TRANSFER_INPUT_REQUEST_MSG);
@@ -44,10 +54,6 @@ public class AccountServiceImpl implements AccountService {
                 }
             }
         }
-    }
-
-    private boolean isCardByNumberPresent(String targetCardNumber) {
-        return cardService.isCardByNumberPresent(targetCardNumber);
     }
 
     @Override
