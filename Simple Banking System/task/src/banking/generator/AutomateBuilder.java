f5e2de2d6fef69b4eb3f14206958885491cd7d5a
Beauclair Ngnintedem <beauclair.dongmo.ngnintedem@cofinpro.de>
2022-01-26 20:20:36 +0100

    CHG: Delegate Automate creation to builder

---
+++ Simple Banking System/task/src/banking/generator/AutomateBuilder.java
@@ -0,0 +1,29 @@
+package banking.generator;
+
+import banking.Automate;
+import banking.repository.CardRepository;
+import banking.services.CardService;
+import banking.services.CardServiceImpl;
+
+public class AutomateBuilder {
+
+    private final String databaseFilename;
+    private CardService cardService;
+
+    private AutomateBuilder(String databaseFilename) {
+        this.databaseFilename = databaseFilename;
+    }
+
+    public static AutomateBuilder init(String databaseFilename) {
+        return new AutomateBuilder(databaseFilename);
+    }
+
+    public AutomateBuilder withCardService() {
+        this.cardService = new CardServiceImpl(CardRepository.init(databaseFilename));
+        return this;
+    }
+
+    public Automate build() {
+        return new Automate(cardService);
+    }
+}
