9e9636c3c57655ba2539072f509945855ba1196b
Beauclair Dongmo Ngnintedem <beauclair.dongmongnintedem@cofinpro.de>
2023-05-31 19:25:58 +0200

    Fix bug

---
+++ Simple Banking System/task/src/banking/util/PropertiesLoader.java
@@ -0,0 +1,41 @@
+package banking.util;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Objects;
+import java.util.Properties;
+
+public class PropertiesLoader {
+
+    private static final PropertiesLoader INSTANCE = new PropertiesLoader();
+
+    private PropertiesLoader() {
+    }
+
+    public static PropertiesLoader getInstance() {
+        return INSTANCE;
+    }
+
+    public Properties properties() {
+        return getProperties("application.properties");
+    }
+
+    public Properties getProperties(String propertiesFilename) {
+        try {
+            return loadProperties(propertiesFilename);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return new Properties();
+    }
+
+    private static Properties loadProperties(String resourceFileName) throws IOException {
+        ClassLoader loader = Thread.currentThread().getContextClassLoader();
+        Properties configuration = new Properties();
+        InputStream inputStream = loader
+                .getResourceAsStream(resourceFileName);
+        configuration.load(inputStream);
+        Objects.requireNonNull(inputStream).close();
+        return configuration;
+    }
+}
